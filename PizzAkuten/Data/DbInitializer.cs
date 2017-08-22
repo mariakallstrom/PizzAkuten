d3a48d531c73f381a2fea6e914f1627620ef949f
mariakallstrom <mariakallstrom@outlook.com>
2017-08-22 09:26:57 +0200

    Add project files.

---
+++ PizzAkuten/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PizzAkuten.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzAkuten.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            var aUser = new ApplicationUser();
+            aUser.UserName = "student@test.se";
+            aUser.Email = "student@test.se";
+            var result = userManager.CreateAsync(aUser, "Passw0rd").Result;
+
+            if (context.Dishes.ToList().Count == 0)
+            {
+                var pizza1 = new Dish
+                {
+                    Name = "Cappricciosa",
+                    Price = 89
+                };
+
+                var pizza2 = new Dish { Name = "Margaritha", Price = 79 };
+                var pizza3 = new Dish { Name = "Vesuvio", Price = 79 };
+                context.AddRange(pizza1, pizza2, pizza3);
+                context.SaveChanges();
+            };
+
+
+
+        }
+    }
+}
---
+++ PizzAkuten/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PizzAkuten.Data;
+using PizzAkuten.Models;
+using PizzAkuten.Services;
+
+namespace PizzAkuten
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("DefaultConnection"));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddTransient<UserManager<ApplicationUser>>();
+            services.AddTransient<DbInitializer>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            DbInitializer.Initialize(context, userManager);
+
+
+        }
+    }
+}
